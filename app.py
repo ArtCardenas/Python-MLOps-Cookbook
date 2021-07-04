acd6422c0ca56977ce4af847d4963e191cdb7e89
Art Cardenas <art_card@yahoo.com>
2021-07-03 21:55:33 -0400

    testing html
    testing html

--- app.py
+++ app.py
@@ -17,14 +17,26 @@ def home():
     html = f"<h3>Predict the Height From Weight of MLB Players</h3>"
     return html.format(format)
 
-@app.route("/predict", methods=['POST','GET'])
+@app.route("/predict", methods=['POST'])
 def predict():
-    """Predicts the Height of MLB Players"""
+    """Predicts the Height of MLB Players JSON"""
     
     json_payload = request.json
     LOG.info(f"JSON payload: {json_payload}")
     prediction = mlib.predict(json_payload['Weight'])
     return jsonify({'prediction': prediction})
+
+@app.route("/predicthtml", methods=['POST','GET'])
+def predicthtml():
+    """Predicts the Height of MLB Players HTML"""
+
+    json_payload = request.json
+    LOG.info(f"JSON payload: {json_payload}")
+    prediction = mlib.predict(json_payload['Weight'])
+    data["Prediction"] = prediction[0]
+    return render_template("output.html",output=data)
+
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080, debug=True)
